ded0062d471774fd2a5cfdc8e3a7e240a0046325
Shawn <coffee.or.tea88@gmail.com>
2019-02-27 19:28:50 +0800

    A simple query. Link schema

--- src/index.js
+++ src/index.js
@@ -3,12 +3,34 @@ const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
 
+// In-memory. TODO: store in database.
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+// all fields for type have resolver
+// Every GraphQL resolver function actually receives four input arguments: parent/root, 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () =>  links
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url
   }
 }
 
